fed1ad574ee63d733ad3c1ced5ca2d8cd716e1d4
thaariqNasution <Thaariqnst@gmail.com>
2023-07-03 14:39:11 +0700

    Genres on Home Page | fixed colors that weren't supported

--- app/src/main/java/com/thaariq/happy5movie/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/thaariq/happy5movie/presentation/home/HomeFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.thaariq.happy5movie.data.responses.GenreItem
+import com.thaariq.happy5movie.data.responses.GenreResponse
 import com.thaariq.happy5movie.databinding.FragmentHomeBinding
+import com.thaariq.happy5movie.presentation.MovieViewModel
+import com.thaariq.happy5movie.presentation.home.adapter.HomeGenreAdapter
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+    private val viewModel by viewModels<MovieViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,7 +23,20 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(layoutInflater)
 
+        viewModel.genre()
+        viewModel.genre.observe(viewLifecycleOwner){
+            setupGenreRV(it)
+        }
         return binding.root
+    }
+
+    private fun setupGenreRV(data : List<GenreItem>){
+        binding.rvCategory.apply {
+            val genreAdapter = HomeGenreAdapter()
+            genreAdapter.setData(data)
+            adapter = genreAdapter
+        }
+
     }
 
 }
